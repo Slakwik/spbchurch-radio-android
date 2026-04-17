ce94b693c6d6a31af2fdf8d2b23fba63f68a98e0
Slakwik <slakwik@example.com>
2026-04-17 13:46:03 +0300

    Fix NowPlayingScreen: add OptIn annotation and fix RepeatOnOne icon

--- android/app/src/main/java/com/spbchurch/radio/ui/screens/player/NowPlayingScreen.kt
+++ android/app/src/main/java/com/spbchurch/radio/ui/screens/player/NowPlayingScreen.kt
@@ -24,6 +24,7 @@ import com.spbchurch.radio.ui.components.*
 import com.spbchurch.radio.ui.theme.Theme
 import com.spbchurch.radio.viewmodel.MainViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NowPlayingScreen(
     viewModel: MainViewModel,
@@ -226,7 +227,7 @@ fun NowPlayingScreen(
                     val orderIcon = when (currentOrder) {
                         PlaybackOrder.SHUFFLE -> Icons.Default.Shuffle
                         PlaybackOrder.REPEAT -> Icons.Default.Repeat
-                        PlaybackOrder.PLAY_ONCE -> Icons.Default.RepeatOnOne
+                        PlaybackOrder.PLAY_ONCE -> Icons.Default.RepeatOne
                     }
 
                     Icon(
